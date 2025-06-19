da0bc046b9f97df34b3beabb75cf86701e24a9ca
worldpark <vbwjsdl@naver.com>
2025-06-19 15:01:18 +0900

    250619 작업

--- src/test/java/com/check/user_check/schedule/AttendanceSchedulerTest.java
+++ src/test/java/com/check/user_check/schedule/AttendanceSchedulerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
+@ActiveProfiles("dev")
 class AttendanceSchedulerTest {
 
     @Autowired
